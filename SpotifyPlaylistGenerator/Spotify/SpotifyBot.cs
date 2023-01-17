d962a90540fd7efca62428883e4673d97d0aca85
bmedrano011 <51801134+bmedrano011@users.noreply.github.com>
2023-01-17 07:42:39 -0500

    feat: get song ids for playlist

--- SpotifyPlaylistGenerator/Spotify/SpotifyBot.cs
+++ SpotifyPlaylistGenerator/Spotify/SpotifyBot.cs
@@ -18,6 +18,8 @@ public class SpotifyBot
     public async Task Run()
     {
         await BuildArtistList();
+        await BuildSongIdList();
+        // await BuildPlaylist();
 
     }
     #endregion
@@ -26,6 +28,7 @@ public class SpotifyBot
     private SpotifyClient _spotify;
     private ILogger<Worker> _logger;
     private Dictionary<string, string>? Artists = new Dictionary<string, string>();
+    private List<string>? SongIds { get; set; } = new List<string>();
     private List<string> Genres = new List<string>()
         {
             "reggaeton",
@@ -41,37 +44,24 @@ public class SpotifyBot
     {
         foreach (var genre in Genres)
         {
-            Console.WriteLine("Genre: {0}", genre);
+            var searchRequest = new SearchRequest(SearchRequest.Types.All, genre);
+            var searchResults = await _spotify.Search.Item(searchRequest);
+
             try
             {
-                Console.WriteLine("Before Search");
-                var searchRequest = new SearchRequest(SearchRequest.Types.All, genre);
-                Console.WriteLine("searchRequest: {0}", searchRequest);
-                var searchResults = await _spotify.Search.Item(searchRequest);
-                Console.WriteLine("searchRequest.ArtistsCount: {0}", searchResults.Artists.Total);
-
                 await foreach (var item in _spotify.Paginate(searchResults.Artists, (s) => s.Artists))
                 {
-                    Console.WriteLine(item.Name);
-                    // you can use "break" here!
+                    if (!Artists.ContainsKey(item.Id))
+                    {
+
+                        Artists.Add(item.Id, item.Name);
+                    }
                 }
-
-                // var index = 0;
-                // // var allResults = await _spotify.PaginateAll(searchResults.Artists, (s) => s.Artists);
-                // foreach (var item in searchResults.Artists.Items)
-                // {
-                //     Console.WriteLine(index);
-                //     Console.WriteLine("Item: {0}", item.Name);
-
-                //     if (!Artists.ContainsKey(item.Id))
-                //     {
-                //         Console.WriteLine("Add Item: {0}", item.Id);
-                //         Console.WriteLine("Item: {0} - {1}", item.Id, item.Name);
-                //         Artists.Add(item.Id, item.Name);
-                //     }
-                //     index++;
-                // }
-
+            }
+            catch (APIException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.Response?.StatusCode);
             }
             catch (Exception ex)
             {
@@ -79,6 +69,77 @@ public class SpotifyBot
             }
 
         }
+    }
+
+    private async Task BuildSongIdList()
+    {
+        if (Artists is not null)
+        {
+            foreach (var art in Artists)
+            {
+                var searchResults = await _spotify.Artists.GetAlbums(art.Key);
+                try
+                {
+                    await foreach (var item in _spotify.Paginate(searchResults))
+                    {
+                        if (DateWithinRange(item.ReleaseDate))
+                        {
+                            var songId = await GetSongId(item.Id, item.Name);
+                            if (!SongIds.Contains(songId) && !String.IsNullOrEmpty(songId))
+                            {
+                                SongIds.Add(songId);
+                            }
+                        }
+                    }
+                }
+                catch (APIException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.Response?.StatusCode);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+        }
+
+    }
+
+    private async Task<string> GetSongId(string albumId, string songName)
+    {
+        string songId = String.Empty;
+        var searchResults = await _spotify.Albums.GetTracks(albumId);
+        try
+        {
+            await foreach (var item in _spotify.Paginate(searchResults))
+            {
+                if (item.Name == songName) songId = item.Id;
+            }
+        }
+        catch (APIException e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.Response?.StatusCode);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+
+        return songId;
+    }
+    private bool DateWithinRange(string date)
+    {
+        DateTime songDate = DateTime.Parse(date);
+        var dateLimit = DateTime.Today.AddDays(-14);
+
+        return (songDate >= dateLimit);
+    }
+
+    private async Task BuildPlaylist()
+    {
+
     }
     #endregion
 
