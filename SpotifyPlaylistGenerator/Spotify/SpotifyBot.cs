197952e322c615a481ad6fa8db6c5bc4cb1f4a33
bmedrano011 <51801134+bmedrano011@users.noreply.github.com>
2023-01-25 21:44:34 -0500

    feat: exclusion logic

--- SpotifyPlaylistGenerator/Spotify/SpotifyBot.cs
+++ SpotifyPlaylistGenerator/Spotify/SpotifyBot.cs
@@ -34,6 +34,7 @@ public class SpotifyBot
     private ILogger<Worker> _logger;
     private List<string> ArtistsNames = new List<string>();
     private List<string> SongURIS = new List<string>();
+    private List<string> AlbumNames = new List<string>();
     private List<string> Genres = new List<string>(){
             "reggaeton",
             "trap latino",
@@ -45,13 +46,42 @@ public class SpotifyBot
             "Dj Cumbio",
             "Reggaeton Latino",
             "Reggaetone",
+            "Reggaetoni",
+            "J Balvin",
+            "Malekhe",
+            "Luis Orlando Ortiz Ibañez",
+            "Reggaeton en VIVO",
+            "Trap Land",
+            "Lofi Hip Hop Nation",
+            "Angelique",
+            "Lo-fi Hip Hop Beats",
+            "Lofi Hip-Hop Music",
+            "Hip Hop 90's",
+            "Latin Music",
+            "Música de Elevador",
+            "No Box Sounds",
+            "Smooth Jazz Beats",
+            "Dinner Jazz Orchestra",
+            "Latin Jazz Vibes",
+            "Lofi Sleep Chill & Study",
+            "Lo Fi Hip Hop",
+            "Lofi Chillhop Gaming Streaming Work Music",
+            "Lofis",
+            "Lofi Night Drives",
+            "loftown",
         };
+
+    private List<string> ExclusionAlbums = new List<string>(){
+        "Re Kgopela Tshireletso"
+    };
+
     private string _playlistID = "25FqRZoYPOXC3qFIMUXppP";
     #endregion
 
     #region Private Methods
     private async Task GetGenreArtists()
     {
+        decimal genreIndex = 1;
         foreach (var genre in Genres)
         {
             Console.WriteLine("Getting Genre {0}", genre);
@@ -63,22 +93,47 @@ public class SpotifyBot
                 decimal index = 1;
                 await foreach (var artist in _spotify.Paginate(searchResults.Artists, (s) => s.Artists))
                 {
-                    if (ExclusionArtists.Find(ea => ea == artist.Name) is not null) continue;
+                    if (artist.Name.ToLower().Contains("lofi") ||
+                    artist.Name.ToLower().Contains("lo-fi") ||
+                    artist.Name.ToLower().Contains("lo fi"))
+                    {
+                        Console.WriteLine($"GetGenreArtists: {artist.Name} - Skipping LOFI");
+                        continue;
 
-                    if (ArtistsNames.Find(a => a == artist.Name) is not null) continue;
+                    }
+                    if (ExclusionArtists.Find(ea => ea == artist.Name) is not null)
+                    {
+                        Console.WriteLine($"GetGenreArtists: {artist.Name} - Excluded");
+                        continue;
+                    }
+
+                    if (ArtistsNames.Find(a => a == artist.Name) is not null)
+                    {
+                        Console.WriteLine($"GetGenreArtists: {artist.Name} - Already processed");
+                        continue;
+                    }
+
+                    if (artist.Popularity < 15)
+                    {
+                        Console.WriteLine($"GetGenreArtists: {artist.Name} - Skipped w/ Popularity:  {artist.Popularity}");
+                        continue;
+                    };
 
                     ArtistsNames.Add(artist.Name);
                     await GetArtistAlbums(artist);
 
+                    decimal? genrePercentage = (genreIndex / Genres.Count()) * 100;
+                    decimal genrePercentageFormatted = decimal.Round(Decimal.Parse(genrePercentage.ToString()), 2, MidpointRounding.AwayFromZero);
                     decimal? percentage = (index / searchResults.Artists.Total) * 100;
                     decimal percentageFormatted = decimal.Round(Decimal.Parse(percentage.ToString()), 2, MidpointRounding.AwayFromZero);
                     index++;
                     if (percentageFormatted % 3 == 0)
                     {
                         Console.WriteLine($"GetGenreArtists: {artist.Name} - Completed:  {percentageFormatted}%");
-
+                        Console.WriteLine($"GetGenreArtists: Genre - Completed:  {genrePercentageFormatted}%");
                     }
                 }
+                genreIndex++;
             }
             catch (APITooManyRequestsException e)
             {
@@ -110,15 +165,25 @@ public class SpotifyBot
                 decimal? percentage = (index / searchResults.Total) * 100;
                 decimal percentageFormatted = decimal.Round(Decimal.Parse(percentage.ToString()), 2, MidpointRounding.AwayFromZero);
                 index++;
-                if (percentageFormatted % 10 == 0)
+                if (percentageFormatted % 12 == 0)
                 {
                     Console.WriteLine($"GetArtistAlbums: {artist.Name} - Completed:  {percentageFormatted}%");
-                    System.Threading.Thread.Sleep(10000);
+                    // System.Threading.Thread.Sleep(10000);
                 }
-
+                if (ExclusionAlbums.Find(ea => ea == album.Name) is not null)
+                {
+                    Console.WriteLine($"GetArtistAlbums: {album.Name} - Excluded");
+                    continue;
+                }
+                if (AlbumNames.Find(a => a == album.Name) is not null)
+                {
+                    Console.WriteLine($"GetArtistAlbums: {album.Name} - Already processed");
+                    continue;
+                }
                 if (AlbumDateWithinRange(album.ReleaseDate) && album.AlbumType != "compilation")
                 {
                     Console.WriteLine("Album {0} within Date Range (21 days) {1} ", album.Name, album.ReleaseDate);
+                    AlbumNames.Add(album.Name);
                     await GetAlbumTracks(album);
                 }
 
@@ -166,7 +231,11 @@ public class SpotifyBot
                 albumTrackUris.Add(track.Uri);
             }
 
-            await AddSongsToPlaylist(albumTrackUris);
+            if (albumTrackUris.Count() > 0)
+            {
+                await AddSongsToPlaylist(albumTrackUris);
+            }
+
         }
         catch (APITooManyRequestsException e)
         {
@@ -259,6 +328,7 @@ public class SpotifyBot
     }
     private async Task AddSongsToPlaylist(List<string> trackURIs)
     {
+        if (trackURIs.Count == 0) return;
         try
         {
             Console.WriteLine($"Adding Tracks to playlist: {trackURIs.Count()} ");
