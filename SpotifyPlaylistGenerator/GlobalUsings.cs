8b81de6c694d2c3d2ad5f35b614dd351fd9f4632
bmedrano011 <51801134+bmedrano011@users.noreply.github.com>
2023-01-26 06:41:18 -0500

    small update

--- SpotifyPlaylistGenerator/GlobalUsings.cs
+++ SpotifyPlaylistGenerator/GlobalUsings.cs
@@ -1,7 +1,6 @@
 global using SpotifyAPI.Web;
 global using Microsoft.Extensions.Configuration;
 global using System.Threading.Tasks;
-global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 global using static SpotifyAPI.Web.Scopes;
